121c54c81644b5c0fc37c10260353e262c4e7c5c
klavior <klavior@gmail.com>
2017-04-05 01:23:18 +0300

    bla

--- app/finger/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from finger.models import UserData
-
-
-class UserDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserData
-        fields = ("username", "details", "user_hash")
